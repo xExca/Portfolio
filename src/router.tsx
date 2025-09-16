fc257632345e4daa9fdc18f292a38765e414ee6e
Onins <naustral@ncmh.gov.ph>
2025-09-16 16:16:04 +0800

    Url fix and item-center in projects

--- src/router.tsx
+++ src/router.tsx
@@ -1,20 +1,20 @@
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutPage from "./Page/AboutPage";
 import ProjectPage from "./Page/ProjectPage";
 import Layout from "./Layout";
 import Project from "./Components/Project";
 
-const router = createHashRouter([
-  {
-    element: <Layout />,
-    children: [
-      { path: "/", element: <AboutPage /> },
-      { path: "/projects", element: <ProjectPage /> },
-      { path: "/project/:id", element: <Project /> },
-    ],
-  },
-]);
-
 export default function AppRoute() {
-  return <RouterProvider router={router} />;
+  const basename = import.meta.env.BASE_URL.replace(/\/$/, "");
+  return (
+    <BrowserRouter basename={basename}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<AboutPage />} />
+          <Route path="/projects" element={<ProjectPage />} />
+          <Route path="/project/:id" element={<Project />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
